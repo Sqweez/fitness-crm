efe7b3b1da93431700b94ebb679d4ab1bd0531c0
Sqweez <sasha.katerinin@gmail.com>
2026-01-12 22:46:46 +0500

    Add ClientSessionV1 and ClientSessionV2 components for enhanced client session management

--- pages/clients/create.vue
+++ pages/clients/create.vue
@@ -1,127 +1,151 @@
 <template>
-  <div>
-    <v-card>
-      <v-card-title>
-        Создание нового клиента
-      </v-card-title>
-      <v-card-text>
-        <v-form ref="form">
-          <v-row>
-            <v-col cols="4">
-              <v-text-field
-                ref="lastname"
-                label="Фамилия"
-                v-model="client.lastname"
-                :rules="[() => !!client.lastname || 'Поле фамилия обязательно для заполнения']"
-                required
-                :error="!!validationErrors.name"
-                :error-messages="validationErrors.name"
-              />
-            </v-col>
-            <v-col cols="4">
-              <v-text-field
-                label="Имя"
-                ref="firstname"
-                v-model="client.firstname"
-                :rules="[() => !!client.firstname || 'Поле имя обязательно для заполнения']"
-                required
-                :error="!!validationErrors.name"
-                :error-messages="validationErrors.name"
-              />
-            </v-col>
-            <v-col cols="4">
-              <v-text-field
-                label="Отчество"
-                ref="patronymic"
-                v-model="client.patronymic"
-                :rules="[() => !!client.patronymic || 'Поле отчество обязательно для заполнения']"
-                required
-                :error="!!validationErrors.name"
-                :error-messages="validationErrors.name"
-              />
-            </v-col>
-            <v-col cols="4">
-              <v-text-field
-                ref="phone"
-                v-model="client.phone"
-                label="Телефон"
-                v-mask="'+7 (###) ### ##-##'"
-                :rules="[
-                () => !!client.phone || 'Поле телефон обязательно для заполнения',
-                () => !!client.phone && client.phone.length === 18 || 'Телефон должен быть заполнен полностью'
-              ]"
-                required
-                :error="!!validationErrors.phone"
-                :error-messages="validationErrors.phone"
-              />
-            </v-col>
-            <v-col cols="2">
-              <v-text-field
-                ref="birth_date"
-                label="Дата рождения"
-                v-model="client.birth_date"
-                v-mask="'##.##.####'"
-                hint="День.месяц.год"
-                :rules="[
-                () => !!client.birth_date || 'Поле дата рождения обязательно для заполнения',
-                () => !!client.birth_date && client.birth_date.length === 10 || 'Дата рождения заполнена некорректно'
-              ]"
-                required
-                :error="!!validationErrors.birth_date"
-                :error-messages="validationErrors.birth_date"
-              />
-            </v-col>
-            <v-col cols="6">
-              <v-radio-group label="Клуб" v-model="client.club_id" row :disabled="!IS_BOSS">
-                <v-radio
-                  v-for="club of clubs"
-                  :key="club.name"
-                  :label="club.name"
-                  :value="club.id" />
-              </v-radio-group>
-            </v-col>
-            <v-col cols="6">
-              <v-text-field
-                v-if="!IS_BOSS"
-                ref="pass"
-                label="Карта"
-                v-model="client.pass"
-                :error="!!validationErrors.pass"
-                :error-messages="validationErrors.pass"
-                :rules="[
-                  () => !!client.pass || 'Поле карта обязательно для заполнения',
-                ]"
-              />
-              <v-text-field
-                v-else
-                ref="pass"
-                label="Карта"
-                v-model="client.pass"
-                hint="Можно оставить поле пустым, если нет необходимости"
-                persistent-hint
-                :error="!!validationErrors.pass"
-                :error-messages="validationErrors.pass"
-              />
-            </v-col>
-            <v-col cols="6" v-show="!isWomenClubChosen">
-              <v-radio-group label="Пол" v-model="client.gender" row ref="gender">
-                <v-radio
-                  v-for="gender of genders"
-                  :key="gender.value"
-                  :label="gender.value"
-                  :value="gender.id" />
-              </v-radio-group>
-            </v-col>
-            <v-col cols="12">
-              <div class="flex items-center gap-x-4 my-4">
-                <div class="w-96 h-96 overflow-hidden" v-show="isClientVideoEnabled">
-                  <video autoplay class="w-full h-full object-cover" ref="webcam"/>
-                </div>
-                <div class="w-96 h-96 overflow-hidden" v-if="photoPreview">
-                  <img :src="photoPreview" class="w-full h-full object-cover">
-                </div>
-                <canvas ref="canvas" width="384" height="384" v-show="false"></canvas>
+  <div class="min-h-[calc(100vh-140px)] bg-gradient-to-br from-slate-50 via-white to-slate-100">
+    <t-card-page title="Создание нового клиента">
+      <v-form ref="form" class="w-full mx-auto">
+        <div class="space-y-6">
+          <div class="rounded-2xl bg-white/80 shadow-sm p-3">
+            <div class="mb-4">
+              <div class="text-xs uppercase tracking-[0.2em] text-slate-500">Основные данные</div>
+              <div class="text-base font-semibold text-slate-800">Профиль клиента</div>
+            </div>
+            <v-row class="px-1">
+              <v-col cols="4">
+                <v-text-field
+                  ref="lastname"
+                  label="Фамилия"
+                  v-model="client.lastname"
+                  :rules="[() => !!client.lastname || 'Поле фамилия обязательно для заполнения']"
+                  required
+                  :error="!!validationErrors.name"
+                  :error-messages="validationErrors.name"
+                />
+              </v-col>
+              <v-col cols="4">
+                <v-text-field
+                  label="Имя"
+                  ref="firstname"
+                  v-model="client.firstname"
+                  :rules="[() => !!client.firstname || 'Поле имя обязательно для заполнения']"
+                  required
+                  :error="!!validationErrors.name"
+                  :error-messages="validationErrors.name"
+                />
+              </v-col>
+              <v-col cols="4">
+                <v-text-field
+                  label="Отчество"
+                  ref="patronymic"
+                  v-model="client.patronymic"
+                  :rules="[() => !!client.patronymic || 'Поле отчество обязательно для заполнения']"
+                  required
+                  :error="!!validationErrors.name"
+                  :error-messages="validationErrors.name"
+                />
+              </v-col>
+              <v-col cols="6">
+                <v-text-field
+                  ref="phone"
+                  v-model="client.phone"
+                  label="Телефон"
+                  v-mask="'+7 (###) ### ##-##'"
+                  maxlength="18"
+                  :rules="[
+                    () => !!client.phone || 'Поле телефон обязательно для заполнения',
+                    () => !!client.phone && client.phone.length === 18 || 'Телефон должен быть заполнен полностью'
+                  ]"
+                  required
+                  :error="!!validationErrors.phone"
+                  :error-messages="validationErrors.phone"
+                />
+              </v-col>
+              <v-col cols="6">
+                <v-text-field
+                  ref="birth_date"
+                  label="Дата рождения"
+                  v-model="client.birth_date"
+                  v-mask="'##.##.####'"
+                  maxlength="10"
+                  hint="День.месяц.год"
+                  :rules="[
+                    () => !!client.birth_date || 'Поле дата рождения обязательно для заполнения',
+                    () => !!client.birth_date && client.birth_date.length === 10 || 'Дата рождения заполнена некорректно'
+                  ]"
+                  required
+                  :error="!!validationErrors.birth_date"
+                  :error-messages="validationErrors.birth_date"
+                />
+              </v-col>
+              <v-col cols="6">
+                <v-select
+                  label="Клуб"
+                  v-model="client.club_id"
+                  :items="clubs"
+                  item-value="id"
+                  item-text="name"
+                  ref="club_id"
+                  :disabled="!IS_BOSS"
+                />
+              </v-col>
+              <v-col cols="6">
+                <v-text-field
+                  v-if="!IS_BOSS"
+                  ref="pass"
+                  label="Карта"
+                  v-model="client.pass"
+                  :error="!!validationErrors.pass"
+                  :error-messages="validationErrors.pass"
+                  :rules="[
+                    () => !!client.pass || 'Поле карта обязательно для заполнения',
+                  ]"
+                />
+                <v-text-field
+                  v-else
+                  ref="pass"
+                  label="Карта"
+                  v-model="client.pass"
+                  hint="Можно оставить поле пустым, если нет необходимости"
+                  persistent-hint
+                  :error="!!validationErrors.pass"
+                  :error-messages="validationErrors.pass"
+                />
+              </v-col>
+              <v-col cols="6" v-show="!isWomenClubChosen">
+                <v-select
+                  label="Пол"
+                  v-model="client.gender"
+                  :items="genders"
+                  item-value="id"
+                  item-text="value"
+                  ref="gender"
+                />
+              </v-col>
+              <v-col cols="12">
+                <v-textarea label="Заметка о клиенте" v-model="client.description"/>
+              </v-col>
+            </v-row>
+          </div>
+
+          <div class="rounded-2xl bg-white/80 shadow-sm p-3">
+            <div class="mb-4 flex items-center justify-between">
+              <div>
+                <div class="text-xs uppercase tracking-[0.2em] text-slate-500">Фото</div>
+                <div class="text-base font-semibold text-slate-800">Портрет клиента</div>
               </div>
+              <div class="text-xs text-slate-500">Загрузите или сделайте фото</div>
+            </div>
+            <div class="flex flex-wrap items-center gap-4">
+              <div class="w-96 h-96 overflow-hidden rounded-2xl border border-slate-200 bg-slate-50" v-show="isClientVideoEnabled">
+                <video autoplay class="w-full h-full object-cover" ref="webcam"/>
+              </div>
+              <div class="w-96 h-96 overflow-hidden rounded-2xl border border-slate-200 bg-white" v-if="photoPreview">
+                <img :src="photoPreview" class="w-full h-full object-contain object-center">
+              </div>
+              <div class="flex-1 min-w-[220px] text-sm text-slate-600">
+                Фото используется в карточке клиента и помогает быстрее идентифицировать человека в списках и расписании.
+              </div>
+              <canvas ref="canvas" width="384" height="384" v-show="false"></canvas>
+            </div>
+            <div class="mt-4 flex flex-wrap gap-3">
               <input type="file" accept="image/*" ref="fileInput" @change="_onFileInputChange" class="hidden">
               <v-btn color="primary" @click="$refs.fileInput.click()">
                 Загрузить фото <Upload :size="18" class="ml-1" />
@@ -129,17 +153,20 @@
               <v-btn color="primary" @click="capturePhoto" v-show="isClientVideoEnabled">
                 Сфотографировать <Camera :size="18" class="ml-1" />
               </v-btn>
-            </v-col>
-            <v-col cols="12">
-              <v-textarea label="Заметка о клиенте" v-model="client.description"/>
-            </v-col>
-          </v-row>
-          <v-btn type="button" color="primary" @click="_onSubmit">
-            Создать <Check :size="18" class="ml-1" />
-          </v-btn>
-        </v-form>
-      </v-card-text>
-    </v-card>
+            </div>
+          </div>
+
+          <div class="flex items-center justify-between bg-white/80 shadow-sm px-3 py-2.5">
+            <div class="text-sm text-slate-600">
+              Проверьте данные перед созданием.
+            </div>
+            <v-btn type="button" color="primary" @click="_onSubmit">
+              Создать <Check :size="18" class="ml-1" />
+            </v-btn>
+          </div>
+        </div>
+      </v-form>
+    </t-card-page>
   </div>
 </template>
 
--- pages/clients/edit/_id.vue
+++ pages/clients/edit/_id.vue
@@ -1,94 +1,121 @@
 <template>
-  <div>
+  <div class="min-h-[calc(100vh-140px)] bg-gradient-to-br from-slate-50 via-white to-slate-100">
     <t-card-page title="Редактирование данных клиента">
-      <v-form ref="form" v-if="IS_CLIENT_LOADED">
-        <v-row>
-          <v-col cols="12" v-show="IS_BOSS">
-            <v-text-field
-              ref="name"
-              label="ФИО"
-              v-model="client.name"
-              :rules="[() => !!client.name || 'Поле ФИО обязательно для заполнения']"
-              required
-              :error="!!validationErrors.name"
-              :error-messages="validationErrors.name"
-            />
-          </v-col>
-          <v-col cols="6">
-            <v-text-field
-              ref="phone"
-              v-model="client.phone"
-              label="Телефон"
-              v-mask="'+7 (###) ### ##-##'"
-              maxlength="18"
-              :rules="[
-                () => !!client.phone || 'Поле телефон обязательно для заполнения',
-                () => !!client.phone && client.phone.length === 18 || 'Телефон должен быть заполнен полностью'
-              ]"
-              required
-              :error="!!validationErrors.phone"
-              :error-messages="validationErrors.phone"
-            />
-          </v-col>
-          <v-col cols="6" v-show="IS_BOSS">
-            <v-text-field
-              ref="birth_date"
-              label="Дата рождения"
-              v-model="client.birth_date_formatted"
-              v-mask="'##.##.####'"
-              maxlength="10"
-              hint="День.месяц.год"
-              :rules="[
-                () => !!client.birth_date || 'Поле дата рождения обязательно для заполнения',
-                () => !!client.birth_date && client.birth_date.length === 10 || 'Дата рождения заполнена некорректно'
-              ]"
-              required
-              :error="!!validationErrors.birth_date"
-              :error-messages="validationErrors.birth_date"
-            />
-          </v-col>
-          <v-col cols="6" v-show="IS_BOSS">
-            <v-select
-              label="Клуб"
-              v-model="client.club.id"
-              :items="clubs"
-              item-value="id"
-              item-text="name"
-              ref="club_id"
-            />
-          </v-col>
-          <v-col cols="6">
-            <v-select
-              label="Пол"
-              v-model="client.gender"
-              :items="genders"
-              item-value="id"
-              item-text="value"
-              ref="gender"
-            />
-          </v-col>
-          <v-col cols="12">
-            <div class="flex items-center gap-x-4 my-4">
-              <div class="w-96 h-96 overflow-hidden" v-show="isClientVideoEnabled">
+      <v-form ref="form" v-if="IS_CLIENT_LOADED" class="w-full mx-auto">
+        <div class="space-y-6">
+          <div class="rounded-2xl  bg-white/80 shadow-sm p-3">
+            <div class="mb-4">
+              <div class="text-xs uppercase tracking-[0.2em] text-slate-500">Основные данные</div>
+              <div class="text-base font-semibold text-slate-800">Профиль клиента</div>
+            </div>
+            <v-row class="px-1">
+              <v-col cols="12" v-show="IS_BOSS">
+                <v-text-field
+                  ref="name"
+                  label="ФИО"
+                  v-model="client.name"
+                  :rules="[() => !!client.name || 'Поле ФИО обязательно для заполнения']"
+                  required
+                  :error="!!validationErrors.name"
+                  :error-messages="validationErrors.name"
+                />
+              </v-col>
+              <v-col cols="6">
+                <v-text-field
+                  ref="phone"
+                  v-model="client.phone"
+                  label="Телефон"
+                  v-mask="'+7 (###) ### ##-##'"
+                  maxlength="18"
+                  :rules="[
+                    () => !!client.phone || 'Поле телефон обязательно для заполнения',
+                    () => !!client.phone && client.phone.length === 18 || 'Телефон должен быть заполнен полностью'
+                  ]"
+                  required
+                  :error="!!validationErrors.phone"
+                  :error-messages="validationErrors.phone"
+                />
+              </v-col>
+              <v-col cols="6" v-show="IS_BOSS">
+                <v-text-field
+                  ref="birth_date"
+                  label="Дата рождения"
+                  v-model="client.birth_date_formatted"
+                  v-mask="'##.##.####'"
+                  maxlength="10"
+                  hint="День.месяц.год"
+                  :rules="[
+                    () => !!client.birth_date || 'Поле дата рождения обязательно для заполнения',
+                    () => !!client.birth_date && client.birth_date.length === 10 || 'Дата рождения заполнена некорректно'
+                  ]"
+                  required
+                  :error="!!validationErrors.birth_date"
+                  :error-messages="validationErrors.birth_date"
+                />
+              </v-col>
+              <v-col cols="6" v-show="IS_BOSS">
+                <v-select
+                  label="Клуб"
+                  v-model="client.club.id"
+                  :items="clubs"
+                  item-value="id"
+                  item-text="name"
+                  ref="club_id"
+                />
+              </v-col>
+              <v-col cols="6">
+                <v-select
+                  label="Пол"
+                  v-model="client.gender"
+                  :items="genders"
+                  item-value="id"
+                  item-text="value"
+                  ref="gender"
+                />
+              </v-col>
+            </v-row>
+          </div>
+
+          <div class="rounded-2xl bg-white/80 shadow-sm p-3">
+            <div class="mb-4 flex items-center justify-between">
+              <div>
+                <div class="text-xs uppercase tracking-[0.2em] text-slate-500">Фото</div>
+                <div class="text-base font-semibold text-slate-800">Портрет клиента</div>
+              </div>
+              <div class="text-xs text-slate-500">Загрузите или сделайте фото</div>
+            </div>
+            <div class="flex flex-wrap items-center gap-4">
+              <div class="w-96 h-96 overflow-hidden rounded-2xl border border-slate-200 bg-slate-50" v-show="isClientVideoEnabled">
                 <video autoplay class="w-full h-full object-cover" ref="webcam"/>
               </div>
-              <div class="w-96 h-96 overflow-hidden" v-if="photoPreview">
+              <div class="w-96 h-96 overflow-hidden rounded-2xl border border-slate-200 bg-white" v-if="photoPreview">
                 <img :src="photoPreview" class="w-full h-full object-contain object-center">
+              </div>
+              <div class="flex-1 min-w-[220px] text-sm text-slate-600">
+                Фото используется в карточке клиента и помогает быстрее идентифицировать человека в списках и расписании.
               </div>
               <canvas ref="canvas" width="384" height="384" v-show="false"></canvas>
             </div>
-            <input type="file" accept="image/*" ref="fileInput" @change="_onFileInputChange" class="hidden">
-            <v-btn color="primary" @click="$refs.fileInput.click()">
-              Загрузить фото <Upload :size="18" class="ml-1" />
+            <div class="mt-4 flex flex-wrap gap-3">
+              <input type="file" accept="image/*" ref="fileInput" @change="_onFileInputChange" class="hidden">
+              <v-btn color="primary" @click="$refs.fileInput.click()">
+                Загрузить фото <Upload :size="18" class="ml-1" />
+              </v-btn>
+              <v-btn color="primary" @click="capturePhoto" v-show="isClientVideoEnabled">
+                Сфотографировать <Camera :size="18" class="ml-1" />
+              </v-btn>
+            </div>
+          </div>
+
+          <div class="flex items-center justify-between bg-white/80 shadow-sm px-3 py-2.5">
+            <div class="text-sm text-slate-600">
+              Проверьте данные перед сохранением.
+            </div>
+            <v-btn type="button" color="primary" @click="_onSubmit">
+              Сохранить <Check :size="18" class="ml-1" />
             </v-btn>
-            <v-btn color="primary" @click="capturePhoto" v-show="isClientVideoEnabled">
-              Сфотографировать <Camera :size="18" class="ml-1" />
-            </v-btn>
-          </v-col>
-        </v-row>
-        <v-btn type="button" class="mt-4" color="primary" @click="_onSubmit">
-          Сохранить <Check :size="18" class="ml-1" />
-        </v-btn>
+          </div>
+        </div>
       </v-form>
     </t-card-page>
   </div>
